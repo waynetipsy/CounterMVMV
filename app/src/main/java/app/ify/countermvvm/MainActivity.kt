901347c82eb1b4a5b1017afbb57d0e62cec76f3a
waynetipsy <jacketoniru@gmail.com>
2024-01-12 17:40:13 +0100

    codebase

---
+++ app/src/main/java/app/ify/countermvvm/MainActivity.kt
@@ -0,0 +1,48 @@
+package app.ify.countermvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.ify.countermvvm.ui.theme.CounterMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CounterMVVMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TheCounterApp() {
+val count = remember {
+    mutableStateOf(0)
+}
+    fun increment() {
+        count.value++
+    }
+
+    fun decrement() {
+       count.value --
+    }
+
+
+}
